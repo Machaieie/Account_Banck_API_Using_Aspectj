612b4ab2eedc68ffc03f2be9eaeee4e173253ff0
Machaieie <machaieieedwin1@gmail.com>
2024-04-20 15:40:26 +0200

    upload project

---
+++ src/main/java/com/contabancaria/Conta_Banciaria/ContaBanciariaApplication.java
@@ -0,0 +1,13 @@
+package com.contabancaria.Conta_Banciaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContaBanciariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContaBanciariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/contabancaria/Conta_Banciaria/ContaBanciariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contabancaria.Conta_Banciaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContaBanciariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
